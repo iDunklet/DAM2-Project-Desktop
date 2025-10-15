612509de075cce84ecdfa39df09a8dba7d3a3b97
Chenhan110 <cxiang2425@politecnics.barcelona>
2025-10-15 17:16:05 +0200

    ggg

--- DAM2-Project-Desktop/Program.cs
+++ DAM2-Project-Desktop/Program.cs
@@ -1,3 +1,5 @@
+using winDesktopForm;
+
 namespace DAM2_Project_Desktop
 {
     internal static class Program
@@ -11,7 +13,8 @@ namespace DAM2_Project_Desktop
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Pantalla7());
         }
     }
 }
+//jyjvfufuyfho
