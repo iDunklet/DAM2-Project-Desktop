396e7ff386b13524d68733e6ca38112df6fed654
Matsailali <matsailali2425@politecnics.barcelona>
2025-11-04 20:25:05 +0100

    actualizacion

--- DAM2-Project-Desktop/Program.cs
+++ DAM2-Project-Desktop/Program.cs
@@ -11,12 +11,8 @@ namespace DAM2_Project_Desktop
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
             Application.Run(new Pantalla8());
-=======
-            Application.Run(new Pantalla5());
->>>>>>> ed2a2af (actualización)
-            //tonto quien lo lea
+
         }
     }
 }
